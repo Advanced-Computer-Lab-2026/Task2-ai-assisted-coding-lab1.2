396b80c6b90a2024b4da15a923efa59318f7352f
jana <janjona294@gmail.com>
2026-09-26 14:03:06 +0300

    Implement event feedback API

--- server/src/routes/feedback.js
+++ server/src/routes/feedback.js
@@ -3,11 +3,14 @@ import {
   getAllFeedbacks,
   getFeedback,
   createFeedback,
-  getFeedbackSummary
+  getFeedbackSummary,
 } from '../controllers/feedbackController.js';
 
 const router = Router();
 
-// TODO: wire up the three routes in README.md section 2 and the summary route in section 3.
+router.get('/summary', getFeedbackSummary);
+router.get('/', getAllFeedbacks);
+router.get('/:id', getFeedback);
+router.post('/', createFeedback);
 
 export default router;
