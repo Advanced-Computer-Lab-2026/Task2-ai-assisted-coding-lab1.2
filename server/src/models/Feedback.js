7b1d7c10c10b57e90fb9e3afccd672c323eed38e
Database Lab <talagado22@gmail.com>
2026-09-26 11:30:44 +0300

    Implement feedback functionality

--- server/src/models/Feedback.js
+++ server/src/models/Feedback.js
@@ -2,12 +2,7 @@ import mongoose from 'mongoose';
 
 // TODO: define the Feedback schema per README.md section 1.
 
-const feedbackSchema = new mongoose.Schema(
-  {
-    // TODO
-  },
-  { timestamps: true }
-);
+const feedbackSchema = new mongoose.Schema( { eventCode: { type: String, required: true, }, score: { type: Number, required: true, min: 1, max: 5, }, comment: { type: String, }, submittedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', }, }, { timestamps: true } );
 
 // TODO: add the compound uniqueness constraint described in README.md section 1.
 
