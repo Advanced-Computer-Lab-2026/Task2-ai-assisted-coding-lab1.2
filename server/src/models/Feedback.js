fc090096dc6ee08fcb1e88e7b4163da58abc6c6a
Jana <janaahmedibrahim15@gmail.com>
2026-09-26 13:54:46 +0300

    Implement event feedback API

--- server/src/models/Feedback.js
+++ server/src/models/Feedback.js
@@ -1,14 +1,32 @@
 import mongoose from 'mongoose';
 
-// TODO: define the Feedback schema per README.md section 1.
-
 const feedbackSchema = new mongoose.Schema(
   {
-    // TODO
+    eventCode: {
+      type: String,
+      required: true,
+    },
+    score: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 5,
+    },
+    comment: {
+      type: String,
+    },
+    submittedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
   },
-  { timestamps: true }
+  {
+    timestamps: true,
+  }
 );
 
-// TODO: add the compound uniqueness constraint described in README.md section 1.
-
+feedbackSchema.index(
+  { eventCode: 1, submittedBy: 1 },
+  { unique: true }
+);
 export const Feedback = mongoose.model('Feedback', feedbackSchema);
